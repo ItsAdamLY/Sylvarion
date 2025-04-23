d2dc42c519235e34e33d64a8c6277c4d4503a727
Richard Mikloš <akisytbsteamy@gmail.com>
2025-04-23 15:32:37 +0200

    feat: migrate chat colors to MiniMessage in SylvATMGUI

--- src/main/java/com/itsadamly/sylvarion/events/ATM/SylvATMGUIOpener.java
+++ src/main/java/com/itsadamly/sylvarion/events/ATM/SylvATMGUIOpener.java
@@ -1,44 +1,46 @@
 package com.itsadamly.sylvarion.events.ATM;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
-public class SylvATMGUIOpener extends SylvATMGUI
-{
-    public SylvATMGUIOpener(String title, Player player)
-    {
-        super(title, player); // "extends" the constructor of SylvATMGUI
+public class SylvATMGUIOpener extends SylvATMGUI {
+    private static final MiniMessage MM = MiniMessage.miniMessage();
+
+    public SylvATMGUIOpener(Component title, Player player) {
+        super(title, player);
     }
 
-    private void openGUI(Inventory menu)
-    {
+    private void openGUI(Inventory menu) {
         player.openInventory(menu);
     }
 
-    public void openATM()
-    {
+    public SylvATMGUIOpener(String miniMessageTitle, Player player) {
+        this(MM.deserialize(miniMessageTitle), player);
+    }
+
+    public void openATM() {
         openGUI(atmMenu());
     }
 
-    public void openTestMenu()
-    {
+    public void openTestMenu() {
         openGUI(testMenu());
     }
 
-    public void openInputCardMenu() { openGUI(inputCardMenu()); }
+    public void openInputCardMenu() {
+        openGUI(inputCardMenu());
+    }
 
-    public void openValuesMenu()
-    {
+    public void openValuesMenu() {
         openGUI(valuesMenu());
     }
 
-    public void openATMOperationsMenu()
-    {
+    public void openATMOperationsMenu() {
         openGUI(atmOperationsMenu());
     }
 
-    public void openConfirmMenu()
-    {
+    public void openConfirmMenu() {
         openGUI(confirmMenu());
     }
 }
