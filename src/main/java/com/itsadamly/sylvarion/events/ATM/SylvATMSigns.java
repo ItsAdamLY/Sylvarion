bf31a6d7ea346d54be6de722448728526657c3c6
Richard Mikloš <akisytbsteamy@gmail.com>
2025-04-23 16:16:24 +0200

    feat: use MiniMessage for ATM sign text color

--- src/main/java/com/itsadamly/sylvarion/events/ATM/SylvATMSigns.java
+++ src/main/java/com/itsadamly/sylvarion/events/ATM/SylvATMSigns.java
@@ -1,22 +1,25 @@
 package com.itsadamly.sylvarion.events.ATM;
 
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 
-public class SylvATMSigns implements Listener
-{
+import java.util.Objects;
+
+public class SylvATMSigns implements Listener {
+    private static final MiniMessage MM = MiniMessage.miniMessage();
     @EventHandler
     public void onSignEdit(SignChangeEvent event)
     {
-        if (event.getLine(0) == null || (event.getLine(0) != null &&
-                !event.getLine(0).equalsIgnoreCase("[ATM]")))
+        if (event.line(0) == null || (event.line(0) != null &&
+                !Objects.requireNonNull(event.line(0)).toString().equalsIgnoreCase("[ATM]")))
             return;
 
         if (!event.getPlayer().hasPermission(""))
             return;
 
-        event.setLine(0, ChatColor.GREEN + "[ATM]");
+        event.line(0, MM.deserialize("<green>[ATM]"));
     }
 }
